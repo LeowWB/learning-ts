dc99af5c41b76ceca65648f7253a91853af7d05e
LeowWB <41581512+LeowWB@users.noreply.github.com>
2018-12-12 20:49:14 +0800

    added some comments to make learning points more memorable

--- greeter.js
+++ greeter.js
@@ -1,4 +1,6 @@
+//ts "compiler" translates to js so any valid js code is also valid in ts
 var Student = /** @class */ (function () {
+    //"public" auto-declares class-level variable
     function Student(firstName, middleInitial, lastName) {
         this.firstName = firstName;
         this.middleInitial = middleInitial;
@@ -11,4 +13,5 @@ function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
 var user = new Student("Jane", "M.", "User");
+user = 42;
 document.body.innerHTML = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,6 +1,10 @@
+//ts "compiler" translates to js so any valid js code is also valid in ts
+//comments are also transferred to the js output file.
+
 class Student {
 	fullName: string;
 
+	//"public" auto-declares class-level variable
 	constructor (public firstName: string, public middleInitial: string, public lastName: string) {
 		this.fullName = firstName + " " + middleInitial + " " + lastName;
 	}
@@ -15,6 +19,17 @@ function greeter(person: Person) {
 	return "Hello, " + person.firstName + " " + person.lastName;
 }
 
+
+//type of variable user is implicitly defined by assignment to an object of type Student
 let user = new Student("Jane", "M.", "User");
+
+//so trying to re-assign it to a value of a different type will give an error.
+//the line below will give a compile error.
+//user = 42;
+
+
+
+
+
 
 document.body.innerHTML = greeter(user);
